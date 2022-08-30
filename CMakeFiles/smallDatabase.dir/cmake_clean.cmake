8c845bc53faee4f6bdd2514b4c06e1ea942983bb
s464982 <michol@st.amu.edu.pl>
2022-08-30 19:47:13 +0200

    dodaj osoby + zapisz osoby

---
+++ CMakeFiles/smallDatabase.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smallDatabase.dir/main.cpp.o"
+  "CMakeFiles/smallDatabase.dir/Osoba.cpp.o"
+  "smallDatabase.pdb"
+  "smallDatabase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smallDatabase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
