f56040340cb640259779084d28a29eba9a1ccc21
Michal Holubowicz <mharcerz@gmail.com>
2022-09-22 16:19:27 +0200

    .

---
+++ CMakeFiles/main.cpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Add.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Add.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/FileOperations.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/FileOperations.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Menu.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Menu.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Person.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Person.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Remove.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Remove.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Search.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Search.cpp.o"
+  "/mnt/c/Users/mharc/Desktop/smallDatabase/Show.cpp" "/mnt/c/Users/mharc/Desktop/smallDatabase/CMakeFiles/main.cpp.dir/Show.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
